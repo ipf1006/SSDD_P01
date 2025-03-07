2c4f8247285425872d07c792a67b2c33c37d37f1
Ignacio <ipf1006@alu.ubu.es>
2025-03-07 17:15:32 +0000

    Inicio de repositorio

---
+++ src/test/java/es/ubu/SSDD_P01/AppTest.java
@@ -0,0 +1,19 @@
+package es.ubu.SSDD_P01;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
